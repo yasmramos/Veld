789fb274f4ea5c589f2bed019f4d22e8daefb2b4
yasmramos <yasmramos95@gmail.com>
2025-12-10 22:42:05 +0800

    Sync with matrix message 343344329695588

--- veld-annotations/src/main/java/module-info.java
+++ veld-annotations/src/main/java/module-info.java
@@ -1,11 +1,8 @@
 /**
  * Veld Annotations Module.
- * Provides all annotations and the Veld API for the Veld DI Framework.
+ * Provides all annotations for the Veld DI Framework.
  */
 module com.veld.annotation {
     // Export annotation package for use by other modules
     exports com.veld.annotation;
-    
-    // Export Veld API (stub that gets replaced by weaver)
-    exports com.veld;
 }
