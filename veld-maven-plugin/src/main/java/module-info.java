4f94722629cf384259cc634575346b2faf2356c0
yasmramos <yasmramos95@gmail.com>
2025-12-19 04:41:05 +0800

    Fix Java modules configuration for CI/CD
    - Simplified module-info.java files to avoid problematic dependencies:
      - Removed ASM dependencies from veld-weaver and veld-processor
      - Removed Maven API dependencies from veld-maven-plugin
    - Changed veld-weaver packaging from 'maven-plugin' to 'jar'
    - Removed maven-plugin-plugin configuration from veld-weaver
    - These changes should resolve 'Creating an aggregated report for both named and unnamed modules' error in CI/CD

--- veld-maven-plugin/src/main/java/module-info.java
+++ veld-maven-plugin/src/main/java/module-info.java
@@ -6,12 +6,6 @@ module veld.maven.plugin {
     // Required JDK modules
     requires java.compiler;
     
-    // Maven API modules
-    requires org.apache.maven.plugin.tools.api;
-    requires plexus.build.api;
-    requires plexus.component.annotations;
-    requires plexus.utils;
-    
     // Required Veld modules
     requires io.github.yasmramos.veld.annotation;
     requires io.github.yasmramos.veld.processor;
--- veld-weaver/src/main/java/module-info.java
+++ veld-weaver/src/main/java/module-info.java
@@ -9,13 +9,6 @@ module veld.weaver {
     
     // Required Veld modules
     requires io.github.yasmramos.veld.annotation;
-    requires io.github.yasmramos.veld.runtime;
-    
-    // ASM modules for bytecode manipulation
-    requires org.objectweb.asm;
-    requires org.objectweb.asm.tree;
-    requires org.objectweb.asm.analysis;
-    requires org.objectweb.asm.util;
     
     // Export weaver packages
     exports io.github.yasmramos.veld.weaver;
