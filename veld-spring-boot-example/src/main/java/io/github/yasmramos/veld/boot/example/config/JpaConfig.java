7ca74e3d05e923889d6aea99a941be17fa26050a
yasmramos <yasmramos95@gmail.com>
2025-12-28 11:59:49 +0100

    fix: update import statement for EntityScan in JpaConfig for consistency

--- veld-spring-boot-example/src/main/java/io/github/yasmramos/veld/boot/example/config/JpaConfig.java
+++ veld-spring-boot-example/src/main/java/io/github/yasmramos/veld/boot/example/config/JpaConfig.java
@@ -1,6 +1,6 @@
 package io.github.yasmramos.veld.boot.example.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
