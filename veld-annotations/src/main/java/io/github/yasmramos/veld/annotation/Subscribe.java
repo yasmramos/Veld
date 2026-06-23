3cecc2388aa42de47c6195063f394e0514f32ce6
yasmramos <yasmramos95@gmail.com>
2026-06-23 19:10:09 +0000

    fix: correct imports and remove unused async classes in starter module

--- veld-annotations/src/main/java/io/github/yasmramos/veld/annotation/Subscribe.java
+++ veld-annotations/src/main/java/io/github/yasmramos/veld/annotation/Subscribe.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.github.yasmramos.veld.processor.annotation;
+package io.github.yasmramos.veld.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
--- veld-processor/src/main/java/io/github/yasmramos/veld/processor/annotation/RequestScoped.java
+++ veld-processor/src/main/java/io/github/yasmramos/veld/processor/annotation/RequestScoped.java
@@ -5,6 +5,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import io.github.yasmramos.veld.annotation.VeldScope;
 
 /**
  * Marks a component as having request scope.
